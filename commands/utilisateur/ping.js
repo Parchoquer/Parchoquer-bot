8dcec7abfb9c24002c6acf6596a5c117b106c859
Parchoquer <bandit44000@gmail.com>
2019-12-03 03:16:41 +0100

    ready.js + ping.js update

--- commands/utilisateur/ping.js
+++ commands/utilisateur/ping.js
@@ -1,4 +1,4 @@
 module.exports = async (client, message) => {
-    const msg = await message.channel.send("ping!");
-    msg.edit(`Pong !\n Latence bot: ${msg.createdTimestamp - message.createdTimestamp}ms.`);
+    const msg = await message.channel.send(":ping_pong: ping!");
+    msg.edit(`:ping_pong: Pong!\n Latence bot: ${msg.createdTimestamp - message.createdTimestamp}ms.`);
 };
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,18 @@
 module.exports = async (client) => {;
+    const activity = [
+        "https://github.com/Parchoquer/Parchoquer-bot",
+        "!help",
+        "CérveauParchoqué.js 🧠#0582",
+        "JavaScript"
+    ];
+
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activity.length - 1) + 1);
+        client.user.setActivity(activity[index]);
+    }, 5000); // 5 sec
+
     console.info(`\x1b[37m\x1b[44m[+]\x1b[0m Tout est opérationnel !`)
-    client.user.setActivity(`!help`, { type: 'PLAYING' })
-    
+
     const channel = client.channels.find(r => r.name === "logs");
     channel.send(":gear: Le bot est redémarré !");
 };
